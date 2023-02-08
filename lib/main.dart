ed2f62100bb444014363b811b6ebdf5651531d17
Gaëlle <gaelle.julien@edu.ece.fr>
2023-02-08 15:37:12 +0100

    Récupération du GTFS RT avec http

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:ppe_rift_test/screens/home_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:gtfs_realtime_bindings/gtfs_realtime_bindings.dart';
 
-void main() {
+
+
+
+void main() async {
+
+  final url = Uri.parse('https://proxy.transport.data.gouv.fr/resource/ilevia-lille-gtfs-rt');
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final feedMessage = FeedMessage.fromBuffer(response.bodyBytes);
+
+    print('Number of entities: ${feedMessage.entity.length}.');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
+
+
   runApp(const MyApp());
 }
 
@@ -11,10 +29,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'PPE RIFT',
-      home: HomeScreen (),
+      theme : ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+      home: const HomeScreen (),
     );
   }
 }
